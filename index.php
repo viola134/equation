bfe2aa87c33ba5b601c0ba29cd75d183220fa71d
viola134 <violapakhomova13@gmail.com>
2022-09-17 19:07:03 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$a = (rand(-10, 10));
+$b = (rand(-10, 10));
+$c = (rand(-10, 10));
+echo ( $a . "x^2n +" . $b .  "x + ". $c  . '=0');
+if  ($d > 0) {
+    $D = sqrt($b*$b-4*$a*$c);
+    $x1 = ((-$b - $D)/2*$a);
+    $x2 = ((-$b + $D)/2*$a);
+    echo ("x1=".$x1);
+    echo ("x2=".$x2);
+}
+elseif ( $d == 0 ){
+    $x = ((-$b)/2*$a);
+    echo ("x=".$x);
+}
+elseif ($d>0){
+    echo "<span style= \"color:red;\"> Нет решений! </span>";
+}
